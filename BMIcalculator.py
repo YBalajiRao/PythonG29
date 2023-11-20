b858b4a5674da2e84ec930629c6883fab57f7a36
Y. Balaji Rao <96159638+YBalajiRao@users.noreply.github.com>
2023-11-20 17:41:26 +0530

    BMIcalculator.py

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -1,17 +1,30 @@
-H=float(input("Enter your height in Centimeters: "))
-W=float(input("Enter your Weight in Kilograms: "))
- 
-the_BMI=W/(H/100)**2
-print("BMI Calculated is:  ",the_BMI)
- 
-if(the_BMI>0):
-    if(the_BMI<=18.5):
-        print("You are underweight")
-    elif(the_BMI<=25):
-        print("Congrats! You are Healthy")
-    elif(the_BMI<=30):
-        print("You are overweight")
-    else: 
-        print("You are Obeset")
+H = float(input("Enter your height in meters: "))
+W = float(input("Enter your weight in kilograms: "))
+gender = input("Enter your gender (male/female): ")
+
+the_BMI = W / (H ** 2)
+print("BMI Calculated is:", the_BMI)
+
+if the_BMI > 0:
+    if gender == 'MALE' or gender == 'male':
+        if the_BMI <= 18.5:
+            print("You are underweight")
+        elif the_BMI <= 25:
+            print("Congrats! You are healthy")
+        elif the_BMI <= 30:
+            print("You are overweight")
+        else:
+            print("You are obese")
+    elif gender == 'FEMALE' or gender == 'female':
+        if the_BMI <= 18.5:
+            print("You are underweight")
+        elif the_BMI <= 24:
+            print("Congrats! You are healthy")
+        elif the_BMI <= 29:
+            print("You are overweight")
+        else:
+            print("You are obese")
+    else:
+        print("Invalid gender entered., pls enter MALE  or FEMALE" )
 else:
-    print("enter valid details")
+    print("Enter valid details")
